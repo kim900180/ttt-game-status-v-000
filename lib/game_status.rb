2f9f8859093f5b0b67464c648fec34d1db762da7
Jae Yong Kim <kim900180@gmail.com>
2018-07-23 00:49:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,8 +19,6 @@ def won?(board)
       return set
     elsif board.all? {|item| item == " "} || board.all? {|item| item == ""}
       return false
-    else
-      return false
     end
   end
 end
