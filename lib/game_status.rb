b27de456a54a4a37fe7de9b83cea6e86bcc01640
Jae Yong Kim <kim900180@gmail.com>
2018-07-23 00:55:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,6 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  win_set = []
   WIN_COMBINATIONS.each do |set|
     if board[set[0]] == "O" && board[set[1]] == "O" &&  board[set[2]] == "O" && !full?(board)
       return set
