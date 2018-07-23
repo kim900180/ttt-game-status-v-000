5e5100182fcf19f4c87b711fcde284911260333a
Jae Yong Kim <kim900180@gmail.com>
2018-07-23 01:01:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,16 +23,16 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 # end
 
 def won?(board)
-  win_index_1 = nil
-  win_index_2 = nil
-  win_index_3 = nil
+  position_1 = nil
+  position_2 = nil
+  position_3 = nil
   
   WIN_COMBINATIONS.each do |set|
-    win_index_1 = set[0]
-    win_index_2 = set[1]
-    win_index_3 = set[2]
+    position_1 = board[set[0]]
+    position_2 = board[set[1]]
+    position_3 = board[set[2]]
   end
-  
+
 end
 
 def full?(board)
