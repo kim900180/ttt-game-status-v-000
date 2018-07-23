75fdf4dc8e4ea64186c50b38776f80e6952f7e33
Jae Yong Kim <kim900180@gmail.com>
2018-07-23 00:35:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,7 +9,7 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 def won?(board)
   win_set = []
   WIN_COMBINATIONS.each do |set|
-    if board[set[0]] == board[set[1]] && board[set[1]] == board[set[2]] && position_taken?(board, set[0])
+    if board[set[0]] == "O" && board[set[1]] == "O" &&  board[set[2]] == "O" || board[set[0]] == "X" && board[set[1]] == "X" &&  board[set[2]] == "X"
       win_set = set
     else
       return false
