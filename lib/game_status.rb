7de6618320c4223838d966e0fc4871e6650e24fa
Jae Yong Kim <kim900180@gmail.com>
2018-07-23 01:09:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,16 +32,16 @@ def won?(board)
     position_1 = board[set[0]]
     position_2 = board[set[1]]
     position_3 = board[set[2]]
-    win_combination = set
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      win_combination = set
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      win_combination = set
     else
       return false
     end
   end
+  return win_combination
 end
 
 def full?(board)
